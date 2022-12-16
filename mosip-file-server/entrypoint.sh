b9166033d15675dffdbba44637c109d7880a1fb6
syed-salman-technoforte <syed.salman@technoforte.co.in>
2022-12-16 17:21:12 +0530

    [ MOSIP-25292 ] Update mosip-file-server
    * updated Dockerfile.
    * updated README.md
    * updated get-cert.sh
    * updated default.conf
    * Added entrypoint.sh
    * Added get-ida-partner-certs.sh

---
+++ mosip-file-server/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp /mnt/mosip-file-server/.well-known/mosipvc/*  /home/mosip/mosip-file-server/.well-known/mosipvc/;
+
+/home/mosip/get-cert.sh;
+
+if [[ $? -gt 0 ]]; then
+  echo "MOSIP's VC PUBLIC KEY Generation failed; EXITING";
+  exit 1;
+fi
+
+/home/mosip/get-ida-partner-certs.sh;
+
+if [[ $? -gt 0 ]]; then
+  echo "Fetch IDA partner certificate failed; EXITING";
+  exit 1;
+fi
+
+echo 'starting nginx'&& nginx;
+sleep infinity;
